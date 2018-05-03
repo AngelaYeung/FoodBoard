6ad5d1b04e9c2c6f4ec3a79cf11836bfae4a1584
Michael Yu <michaelmhyu94@gmail.com>
2018-05-03 14:52:55 -0700

    connected database to register function, database to add item function, fixed simple styling in index.css

--- boardpage.js
+++ boardpage.js
@@ -1,41 +1,7 @@
-// Creates a thumbnail when an image has been uploaded
-function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
-
-    // Only process image files.
-    if (files[0].type.match('image.*')) {
-
-
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function (theFile) {
-            return function (e) {
-                // Render thumbnail.
-                var span = document.createElement('span');
-                span.innerHTML = ['<img class="thumb" src="', e.target.result,
-                    '" title="', escape(theFile.name), '"/>'].join('');
-                document.getElementById("output").appendChild(span);
-            };
-        })(files[0]);
-
-        // Read in the image file as a data URL.
-        reader.readAsDataURL(files[0]);
-    }
-
-}
-
-document.getElementById('file-input').addEventListener('change', handleFileSelect, false);
-
-
-//Event handler for when the submit button is clicked.
-//Should post the contents of the form onto the board.
-$("#submit").click(function () {
-    addNewItem();
-});
-
-//Function for posting the user input onto the board
-function addNewItem() {
+/**
+ * Creates new card
+ */
+function addNewItem(id, name, description, foodGroup, dateTime, img) {
 
     var cardDiv = document.createElement("div");
     cardDiv.setAttribute("id", "card");
@@ -52,34 +18,32 @@ function addNewItem() {
     textDiv.setAttribute("class", "col-xs-10");
     
     var foodName = document.createElement("h4");
+
     // grabs the name from the form so that it will be appended to cardDiv
-    foodName.innerHTML = $("#name").val();
+    foodName.innerHTML = name;
 
     var buttonDiv = document.createElement("div");
     buttonDiv.setAttribute("class", "col-xs-2");
     
     var toggleButton = document.createElement("button");
     toggleButton.setAttribute("data-toggle", "collapse");
-    toggleButton.setAttribute("data-target", "#collapseDiv");
+    toggleButton.setAttribute("data-target", `#collapseDiv${id}`);
     toggleButton.setAttribute("class", "collapse-button");
 
     var toggleDiv = document.createElement("div");
-    toggleDiv.setAttribute("id", "collapseDiv");
+    toggleDiv.setAttribute("id", `collapseDiv${id}`);
     toggleDiv.setAttribute("class", "collapse");
+
     //takes the contents of the description 
     var foodDescription = document.createElement("p");
-    foodDescription.innerHTML = $("#description").val();
+    foodDescription.innerHTML = description;
     
     var imageDiv = document.createElement("div");
     imageDiv.setAttribute("class","imgDiv");
 
     var foodImg = document.createElement("img");
     foodImg.setAttribute("class", "food-img");
-    foodImg.src = "./Pictures/hero-image-flat.jpg";
-
-    var claimButton = document.createElement("button");
-    claimButton.setAttribute("class", "claim");
-    
+    foodImg.src = `/images/${img}`;
 
     //$("#datetimepicker").datetimepicker();
     //cardDiv.appendChild(foodImg);
@@ -97,17 +61,13 @@ function addNewItem() {
     textDiv.appendChild(foodName);
 
     toggleDiv.appendChild(foodDescription);
-    $("#board").append(cardDiv);
+    $("#card-list").append(cardDiv);
+
+    /** Clearing Forms */
+    $('#postForm').trigger('reset');
+
+    /** Hides Modal */
+    if($('#itemModal').hasClass('show')) {
+        $('#itemModal').modal('toggle');
+    }
 }
-
-// function imageReader(file) {
-//     var reader = new FileReader();
-
-//     return imgSRC = window.URL.createObjectURL(imgObj);
-
-// }
-
-//Will take input received the form and create a JSON object.
-// function createFoodObject() {
-
-// }
