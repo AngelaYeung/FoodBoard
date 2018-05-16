8d36630cfe17924a9a6c9b5639997ea9956f34e5
Michael Yu <michaelmhyu94@gmail.com>
2018-05-16 16:52:56 -0700

    tried to fix post item return to handle dynamically on server (claim and delete button for users not rendering properly until refreshed.

--- app/public/js/boardpage.js
+++ app/public/js/boardpage.js
@@ -156,210 +156,216 @@ $(document).ready(function () {
   });
 
   socket.on('post item return', (item) => {
-    createCardNoClaim(item.id, item.name, item.description, item.dateTime, item.foodgrouping, item.image);
+    if (rows[i].Users_userID === userID) {
+      createCardNoClaim(rows[i].itemID, rows[i].foodName, rows[i].foodDescription, rows[i].foodExpiryTime,
+        rows[i].foodGroup, rows[i].foodImage);
+    } else {
+      createCardNoDelete(rows[i].itemID, rows[i].foodName, rows[i].foodDescription, rows[i].foodExpiryTime,
+        rows[i].foodGroup, rows[i].foodImage);
+    }
   });
 
-  /*************************************************************************
-   * 
-   *         FOOD BOARD LOAD FEATURE - CLIENT SIDE
-   * 
-   *************************************************************************/
+    /*************************************************************************
+     * 
+     *         FOOD BOARD LOAD FEATURE - CLIENT SIDE
+     * 
+     *************************************************************************/
 
-  /**
-   * When the window is loaded, trigger websocket event for server to fetch foodboard posts
-   * from the data base. 
-   */
-  $(window).on('load', () => {
+    /**
+     * When the window is loaded, trigger websocket event for server to fetch foodboard posts
+     * from the data base. 
+     */
+    $(window).on('load', () => {
 
-    console.log('Client: page loaded:', sessionID);
-    socket.emit('page loaded', {
-      sessionID: sessionID,
+      console.log('Client: page loaded:', sessionID);
+      socket.emit('page loaded', {
+        sessionID: sessionID,
+      });
+    });
+
+    socket.on('load foodboard', (items) => {
+      var role = items.role; // their role as administrator or user
+      var userID = items.userID; // whos logged in
+      var rows = items.rows;
+      console.log("LOAD: ROWS: ", rows);
+      for (var i = 0; i < rows.length; i++) {
+        console.log('userID: ', userID);
+        console.log(`rows[${i}].Users_user: `, rows[i].Users_userID);
+        if (role === 0) {
+          if (rows[i].Users_userID === userID) {
+            createCardNoClaim(rows[i].itemID, rows[i].foodName, rows[i].foodDescription, rows[i].foodExpiryTime,
+              rows[i].foodGroup, rows[i].foodImage);
+          } else {
+            createCardBothButtons(rows[i].itemID, rows[i].foodName, rows[i].foodDescription, rows[i].foodExpiryTime,
+              rows[i].foodGroup, rows[i].foodImage);
+          }
+        } else {
+          if (rows[i].Users_userID === userID) {
+            createCardNoClaim(rows[i].itemID, rows[i].foodName, rows[i].foodDescription, rows[i].foodExpiryTime,
+              rows[i].foodGroup, rows[i].foodImage);
+          } else {
+            createCardNoDelete(rows[i].itemID, rows[i].foodName, rows[i].foodDescription, rows[i].foodExpiryTime,
+              rows[i].foodGroup, rows[i].foodImage);
+          }
+        }
+      }
+    });
+
+    socket.on('myposts', (items) => {
+      let userID = items.userID;
+      let rows = items.rows;
+      for (let i = 0; i < rows.length; i++) {
+        createCardNoClaim(rows[i].itemID, rows[i].foodName, rows[i].foodDescription, rows[i].foodExpiryTime, rows[i].foodGroup, rows[i].foodImage);
+      }
+    });
+
+    /*************************************************************************
+     * 
+     *         FOOD BOARD DELETE FEATURE - CLIENT SIDE
+     * 
+     *************************************************************************/
+    socket.on('delete return', (itemID) => {
+      itemDeleted(itemID); //deletes the item
+    });
+
+    /*************************************************************************
+     * 
+     *         FOOD BOARD CLAIM FEATURE - CLIENT SIDE
+     * 
+     *************************************************************************/
+
+    socket.on('claim return', (itemID) => {
+      itemClaimed(itemID);
+    });
+
+    /************************************************
+  * 
+  *              SEARCH FEATURE
+  * 
+  *************************************************/
+
+    /**
+     * Handles search bar clear button toggle
+     */
+    $('#search-bar').on('keyup', (event) => {
+      console.log('searchbar');
+      if ($('#search-bar').val() !== '') {
+        $('#search-bar-btn-reset').show();
+      } else {
+        $('#search-bar-btn-reset').hide();
+      }
+    });
+
+    $('#search-bar-btn-reset').on('click', () => {
+      $('#search-bar-btn-reset').hide();
+    });
+
+    $(window).on('scroll', () => {
+      if ($(window).scrollTop() < 2) {
+        $('#search-bar-container').slideDown(150);
+        $('#card-list').animate({ 'margin-top': '2%', 'padding-top': '0%' }, 25, 'linear');
+        $('#search-bar-container').animate({ 'padding-top': '15%' }, 25, 'linear');
+
+      } else {
+        $('#search-bar-container').slideUp(150);
+        $('#search-bar-container').animate({ 'padding-top': '15%' }, 25, 'linear');
+        $('#card-list').animate({ 'margin-top': '10%', 'padding-top': '2%' }, 25, 'linear');
+      }
+    });
+
+    $('#search-bar-form').on('submit', (event) => {
+      event.preventDefault();
     });
   });
 
-  socket.on('load foodboard', (items) => {
-    var role = items.role; // their role as administrator or user
-    var userID = items.userID; // whos logged in
-    var rows = items.rows;
-    console.log("LOAD: ROWS: ", rows);
-    for (var i = 0; i < rows.length; i++) {
-      console.log('userID: ', userID);
-      console.log(`rows[${i}].Users_user: `, rows[i].Users_userID);
-      if (role === 0) {
-        if (rows[i].Users_userID === userID) {
-          createCardNoClaim(rows[i].itemID, rows[i].foodName, rows[i].foodDescription, rows[i].foodExpiryTime,
-            rows[i].foodGroup, rows[i].foodImage);
-        } else {
-          createCardBothButtons(rows[i].itemID, rows[i].foodName, rows[i].foodDescription, rows[i].foodExpiryTime,
-            rows[i].foodGroup, rows[i].foodImage);
-        }
-      } else {
-        if (rows[i].Users_userID === userID) {
-          createCardNoClaim(rows[i].itemID, rows[i].foodName, rows[i].foodDescription, rows[i].foodExpiryTime,
-            rows[i].foodGroup, rows[i].foodImage);
-        } else {
-          createCardNoDelete(rows[i].itemID, rows[i].foodName, rows[i].foodDescription, rows[i].foodExpiryTime,
-            rows[i].foodGroup, rows[i].foodImage);
-        }
-      }
-    }
-  });
-
-  socket.on('myposts', (items) => {
-    let userID = items.userID;
-    let rows = items.rows;
-    for (let i = 0; i < rows.length; i++) {
-      createCardNoClaim(rows[i].itemID, rows[i].foodName, rows[i].foodDescription, rows[i].foodExpiryTime, rows[i].foodGroup, rows[i].foodImage);
-    }
-  });
-
-  /*************************************************************************
-   * 
-   *         FOOD BOARD DELETE FEATURE - CLIENT SIDE
-   * 
-   *************************************************************************/
-  socket.on('delete return', (itemID) => {
-    itemDeleted(itemID); //deletes the item
-  });
-
-  /*************************************************************************
-   * 
-   *         FOOD BOARD CLAIM FEATURE - CLIENT SIDE
-   * 
-   *************************************************************************/
-
-  socket.on('claim return', (itemID) => {
-    itemClaimed(itemID);
-  });
-
-  /************************************************
-* 
-*              SEARCH FEATURE
-* 
-*************************************************/
 
   /**
-   * Handles search bar clear button toggle
+   * Removes the claimed items from the board.
+   * @param {number} id 
    */
-  $('#search-bar').on('keyup', (event) => {
-    console.log('searchbar');
-    if ($('#search-bar').val() !== '') {
-      $('#search-bar-btn-reset').show();
-    } else {
-      $('#search-bar-btn-reset').hide();
-    }
-  });
+  function itemClaimed(id) {
+    $(`#card${id}`).remove();
+  };
 
-  $('#search-bar-btn-reset').on('click', () => {
-    $('#search-bar-btn-reset').hide();
-  });
+  /**
+   * Sends emits the item id to the server.
+   * @param {number} itemID 
+   */
+  function claimItem(itemID) {
+    let sessionID = getSessionID('connect.sid');
+    socket.emit('claim item', {
+      id: itemID,
+      sessionID: sessionID,
+    });
+  };
 
-  $(window).on('scroll', () => {
-    if ($(window).scrollTop() < 2) {
-      $('#search-bar-container').slideDown(150);
-      $('#card-list').animate({ 'margin-top': '2%', 'padding-top' : '0%' }, 25, 'linear');
-      $('#search-bar-container').animate({'padding-top' : '15%'}, 25, 'linear');
-
-    } else {
-      $('#search-bar-container').slideUp(150);
-      $('#search-bar-container').animate({'padding-top' : '15%'}, 25, 'linear');
-      $('#card-list').animate({ 'margin-top': '10%', 'padding-top' : '2%' }, 25, 'linear');
-    }
-  });
-
-  $('#search-bar-form').on('submit', (event) => {
-    event.preventDefault();
-  });
-});
-
-
-/**
- * Removes the claimed items from the board.
- * @param {number} id 
- */
-function itemClaimed(id) {
-  $(`#card${id}`).remove();
-};
-
-/**
- * Sends emits the item id to the server.
- * @param {number} itemID 
- */
-function claimItem(itemID) {
-  let sessionID = getSessionID('connect.sid');
-  socket.emit('claim item', {
-    id: itemID,
-    sessionID: sessionID,
-  });
-};
-
-function deleteItem(itemID) {
-  let sessionID = getSessionID('connect.sid');
-  socket.emit('delete item', {
-    id: itemID,
-    sessionID: sessionID
-  });
-}
-
-/**
- * Gets the session id. 
- * @param {string} name - name of the cookie session key we are grabbing (should be connect.sid)
- */
-function getSessionID(name) {
-  var cookie = getCookie(name);
-  var sessionID = cookie.substring(4, cookie.lastIndexOf('.'));
-  return sessionID;
-}
-
-
-/**
- * Gets the cookie of the particular session.
- * @param {string} name 
- */
-function getCookie(name) {
-  var value = "; " + document.cookie;
-  var parts = value.split("; " + name + "=");
-  if (parts.length == 2) {
-    return parts.pop().split(";").shift();
+  function deleteItem(itemID) {
+    let sessionID = getSessionID('connect.sid');
+    socket.emit('delete item', {
+      id: itemID,
+      sessionID: sessionID
+    });
   }
-};
 
-function setPostImage(foodCategory, imgName) {
-  if (imgName !== "undefined.png") {
-    return `/images/${imgName}`;
-  } else {
-    switch (foodCategory) {
-      case "Produce":
-        return "../../Pictures/default_produce.png";
-        break;
-      case "Meat":
-        return "../../Pictures/default_meat.png";
-        break;
-      case "Canned Goods":
-        return "../../Pictures/default_food.png";
-        break;
-      case "Packaged":
-        return "../../Pictures/default_packaged.png";
-        break;
+  /**
+   * Gets the session id. 
+   * @param {string} name - name of the cookie session key we are grabbing (should be connect.sid)
+   */
+  function getSessionID(name) {
+    var cookie = getCookie(name);
+    var sessionID = cookie.substring(4, cookie.lastIndexOf('.'));
+    return sessionID;
+  }
+
+
+  /**
+   * Gets the cookie of the particular session.
+   * @param {string} name 
+   */
+  function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) {
+      return parts.pop().split(";").shift();
+    }
+  };
+
+  function setPostImage(foodCategory, imgName) {
+    if (imgName !== "undefined.png") {
+      return `/images/${imgName}`;
+    } else {
+      switch (foodCategory) {
+        case "Produce":
+          return "../../Pictures/default_produce.png";
+          break;
+        case "Meat":
+          return "../../Pictures/default_meat.png";
+          break;
+        case "Canned Goods":
+          return "../../Pictures/default_food.png";
+          break;
+        case "Packaged":
+          return "../../Pictures/default_packaged.png";
+          break;
+      }
     }
   }
-}
 
-function itemDeleted(id) {
-  $(`#card${id}`).remove();
-}
+  function itemDeleted(id) {
+    $(`#card${id}`).remove();
+  }
 
-/**
- * Creates Card from FoodItem Table without a 'Claim' Button.
- * @param {*} id 
- * @param {*} name 
- * @param {*} description 
- * @param {*} dateTime 
- * @param {*} foodGroup 
- * @param {*} img 
- */
-function createCardNoClaim(id, name, description, dateTime, foodGroup, img) {
-  $('#card-list').prepend(`
+  /**
+   * Creates Card from FoodItem Table without a 'Claim' Button.
+   * @param {*} id 
+   * @param {*} name 
+   * @param {*} description 
+   * @param {*} dateTime 
+   * @param {*} foodGroup 
+   * @param {*} img 
+   */
+  function createCardNoClaim(id, name, description, dateTime, foodGroup, img) {
+    $('#card-list').prepend(`
   <div id="card${id}" class="cardContainer">
     <div class="imgDiv">
         <img class="food-img" src="${setPostImage(foodGroup, img)}">
@@ -387,29 +393,21 @@ function createCardNoClaim(id, name, description, dateTime, foodGroup, img) {
             <p></p>
         </div>
     </div>`);
-  /** Clearing Forms */
-  $('#postForm').trigger('reset');
-}
+    /** Clearing Forms */
+    $('#postForm').trigger('reset');
+  }
 
-function formatDate(dateTime) {
-  var expiryDate = new Date(dateTime);
-  console.log('Expiry date', expiryDate);
-  var today = new Date(Date.now());
-  console.log('today', today);
-  var formatedDate = moment(expiryDate).fromNow();
-  return formatedDate;
-};
-/**
- * Creates Card from FoodItem Table without a 'Delete' Button.
- * @param {*} id 
- * @param {*} name 
- * @param {*} description 
- * @param {*} dateTime 
- * @param {*} foodGroup 
- * @param {*} img 
- */
-function createCardNoDelete(id, name, description, dateTime, foodGroup, img) {
-  $('#card-list').prepend(`
+  /**
+   * Creates Card from FoodItem Table without a 'Delete' Button.
+   * @param {*} id 
+   * @param {*} name 
+   * @param {*} description 
+   * @param {*} dateTime 
+   * @param {*} foodGroup 
+   * @param {*} img 
+   */
+  function createCardNoDelete(id, name, description, dateTime, foodGroup, img) {
+    $('#card-list').prepend(`
   <div id="card${id}" class="cardContainer">
     <div class="imgDiv">
         <img class="food-img" src="${setPostImage(foodGroup, img)}">
@@ -437,21 +435,21 @@ function createCardNoDelete(id, name, description, dateTime, foodGroup, img) {
             <p></p>
         </div>
     </div>`);
-  /** Clearing Forms */
-  $('#postForm').trigger('reset');
-}
+    /** Clearing Forms */
+    $('#postForm').trigger('reset');
+  }
 
-/**
- * Creates Card from FoodItem Table with both buttons.
- * @param {*} id 
- * @param {*} name 
- * @param {*} description 
- * @param {*} dateTime 
- * @param {*} foodGroup 
- * @param {*} img 
- */
-function createCardBothButtons(id, name, description, dateTime, foodGroup, img) {
-  $('#card-list').prepend(`
+  /**
+   * Creates Card from FoodItem Table with both buttons.
+   * @param {*} id 
+   * @param {*} name 
+   * @param {*} description 
+   * @param {*} dateTime 
+   * @param {*} foodGroup 
+   * @param {*} img 
+   */
+  function createCardBothButtons(id, name, description, dateTime, foodGroup, img) {
+    $('#card-list').prepend(`
   <div id="card${id}" class="cardContainer">
     <div class="imgDiv">
         <img class="food-img" src="${setPostImage(foodGroup, img)}">
@@ -478,9 +476,18 @@ function createCardBothButtons(id, name, description, dateTime, foodGroup, img) 
             <p></p>
         </div>
     </div>`);
-  /** Clearing Forms */
-  $('#postForm').trigger('reset');
-}
+    /** Clearing Forms */
+    $('#postForm').trigger('reset');
+  }
+
+  function formatDate(dateTime) {
+    var expiryDate = new Date(dateTime);
+    console.log('Expiry date', expiryDate);
+    var today = new Date(Date.now());
+    console.log('today', today);
+    var formatedDate = moment(expiryDate).fromNow();
+    return formatedDate;
+  };
 
 // Creates a thumbnail when an image has been uploaded
 // function handleFileSelect(evt) {
