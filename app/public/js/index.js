0339cfdd548a43fa5990b34ce9397160795b730f
Derek Lee <dlee477@my.bcit.ca>
2018-05-22 17:06:29 -0700

    back button closes modal instead of going back a page

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -13,6 +13,19 @@ document.onreadystatechange = function () {
 }
 
 $(document).ready(function () {
+  //close modals with back btn
+$('.modal').on('show.bs.modal', function (e) {
+  window.history.pushState('forward', null, '#modal');
+});
+
+$('.modal').on('hide.bs.modal', function (e) {
+  //pop the forward state to go back to original state before pushing the "Modal!" button
+});
+
+$(window).on('popstate', function () {
+  $('.modal').modal('hide');
+});
+
 
   // add scrollspy onto body of the page for about link
   $("#about-link").click(function () {
@@ -79,10 +92,6 @@ $(document).ready(function () {
       }
     });
 
-    //closes modal when clicking back button
-    $(window).on("popstate", this.handleBackpress);
-    document.addEventListener("backbutton", this.handleBackpress, false);
-
   });
 
   //change navbar colour green when opening dropdown
@@ -129,9 +138,6 @@ $("#newPW,#confirmPW").on('change', (e) => {
   }
 });
 
-
-
-
 //validate the password
 function validatePassword() {
   if ($("#register_pwd").val() !== $("#register_pwd_confirm").val()) {
@@ -141,12 +147,3 @@ function validatePassword() {
   }
 }
 
-
-//closes modal when clicking back button
-function handleBackpress(e) {
-  e.preventDefault();
-  e.stopPropagation();
-
-  $(".modal").modal("hide");
-  $(".modal-backdrop").remove();
-}
